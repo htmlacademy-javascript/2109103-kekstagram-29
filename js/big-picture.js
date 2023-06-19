73a3208922e4946e6084913920cdf18522c46dd5
UberSurale <fazon.tropol@gmail.com>
2023-06-19 20:53:09 +0700

    Исправляет генерацию данных

---
+++ js/big-picture.js
@@ -0,0 +1,2 @@
+const userBigPicture = document.querySelector('.big-picture');
+const userBigPictureClose = userBigPicture.querySelector('.cancel');
--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 import './util.js';
 import './data.js';
 import { getRandomDescriptions } from './data.js';
-import './picture.js';
+import './miniature-list.js';
+import './big-picture.js';
 
 console.log (
   getRandomDescriptions()
