4b3bf3232be955337d9b61551ee81f27bb161daf
UberSurale <fazon.tropol@gmail.com>
2023-06-19 21:27:11 +0700

    Начинает рефакторинг обработчиков

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,2 +1,45 @@
+import './miniature-list.js';
+import { isEscapeKey } from './util.js';
+
 const userBigPicture = document.querySelector('.big-picture');
+
+const miniatureList = document.querySelector('.pictures');
+
 const userBigPictureClose = userBigPicture.querySelector('.cancel');
+
+function openUserModal() {
+  userBigPicture.classList.remove('hidden');
+
+  document.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      userBigPicture.classList.add('hidden');
+    }
+  });
+}
+
+function closeUserModal() {
+  userBigPicture.classList.add('hidden');
+
+  document.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      userBigPicture.classList.add('hidden');
+    }
+  });
+}
+
+miniatureList.addEventListener('click', () => {
+  openUserModal();
+});
+
+userBigPictureClose.addEventListener('click', () => {
+  closeUserModal();
+});
+
+// document.addEventListener('keydown', (evt) => {
+//   if (isEscapeKey(evt)) {
+//     evt.preventDefault();
+//     userBigPicture.classList.add('hidden');
+//   }
+// });
--- js/main.js
+++ js/main.js
@@ -2,10 +2,5 @@
 
 import './util.js';
 import './data.js';
-import { getRandomDescriptions } from './data.js';
 import './miniature-list.js';
 import './big-picture.js';
-
-console.log (
-  getRandomDescriptions()
-)
