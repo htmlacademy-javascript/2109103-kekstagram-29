b4c3acdfd73a75814fa3941e2d081d2afa4da425
UberSurale <fazon.tropol@gmail.com>
2023-06-27 13:30:05 +0700

    Завершает работау над показать ещё

--- js/miniature-list.js
+++ js/miniature-list.js
@@ -1,19 +1,51 @@
-import { bigPictureComments, bigPictureCommentsCount, bigPictureLikes, openUserModal, userBigPictureImage } from './big-picture.js';
+import { userBigPicture, bigPictureComments, bigPictureCommentsCount, bigPictureLikes, openUserModal, userBigPictureImage } from './big-picture.js';
 import { getRandomDescriptions } from './data.js';
 
 const picturesList = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const showMore = document.querySelector('.comments-loader');
 const pictureComment = bigPictureComments.querySelector('.social__comment');
-
+const commentCountFromTo = userBigPicture.querySelector('.social__comment-count');
 
 const usersPictures = getRandomDescriptions();
 
+let limitedComments = [];
+
+let currentPictureComments = [];
+
 let displayedComments = 5;
+
+const renderComments = () => {
+  limitedComments = currentPictureComments.slice(0, displayedComments);
+  const commentFragment = document.createDocumentFragment();
+
+  limitedComments.forEach(({ avatar, name, message }) => {
+    const commentElement = pictureComment.cloneNode(true);
+
+    const pictureCommentImage = commentElement.querySelector('.social__picture');
+    const pictureCommentText = commentElement.querySelector('.social__text');
+
+    pictureCommentImage.src = avatar;
+    pictureCommentImage.alt = name;
+    pictureCommentText.textContent = message;
+
+    commentFragment.append(commentElement);
+  });
+
+  bigPictureComments.innerHTML = '';
+  bigPictureComments.append(commentFragment);
+
+  commentCountFromTo.textContent = `${displayedComments} из ${currentPictureComments.length} комментариев`;
+
+  if (displayedComments >= currentPictureComments.length) {
+    showMore.style.display = 'none';
+  } else {
+    showMore.style.display = 'block';
+  }
+};
 
 const renderMiniatures = () => {
   const picturesFragment = document.createDocumentFragment();
-  const commentFragment = document.createDocumentFragment();
 
   usersPictures.forEach(({ url, description, likes, comment }) => {
     const picturesElement = picturesTemplate.cloneNode(true);
@@ -28,22 +60,7 @@ const renderMiniatures = () => {
 
     picturesList.appendChild(picturesElement);
 
-    const renderComments = () => {
-      comment.slice(0, 5).forEach(({ avatar, name, message }) => {
-        const commentElement = pictureComment.cloneNode(true);
-
-        const pictureCommentImage = commentElement.querySelector('.social__picture');
-        const pictureCommentText = commentElement.querySelector('.social__text');
-
-        pictureCommentImage.src = avatar;
-        pictureCommentImage.alt = name;
-        pictureCommentText.textContent = message;
-
-        commentFragment.append(commentElement);
-      });
-    };
-
-    picturesElement.addEventListener('click', (evt) => {
+    const onMiniatureClick = (evt) => {
       openUserModal();
       renderComments();
 
@@ -54,15 +71,36 @@ const renderMiniatures = () => {
         bigPictureLikes.textContent = picturesLikes.textContent;
         bigPictureCommentsCount.textContent = pictureComments.textContent;
 
-        bigPictureComments.innerHTML = '';
-        bigPictureComments.append(commentFragment);
+        currentPictureComments = comment;
+        if (currentPictureComments.length <= 5) {
+          displayedComments = currentPictureComments.length;
+        } else {
+          displayedComments = 5;
+        }
+        renderComments();
       }
-    });
+
+      picturesElement.removeEventListener('click', onMiniatureClick);
+    };
+
+    picturesElement.addEventListener('click', onMiniatureClick);
   });
 
   picturesList.appendChild(picturesFragment);
 };
 
+const onShowMoreClick = () => {
+  displayedComments += 5;
+
+  if (displayedComments > currentPictureComments.length) {
+    displayedComments = currentPictureComments.length;
+  }
+
+  renderComments();
+};
+
+showMore.addEventListener('click', onShowMoreClick);
+
 renderMiniatures();
 
-export {renderMiniatures, picturesList};
+export {renderMiniatures, picturesList, onShowMoreClick, showMore};
